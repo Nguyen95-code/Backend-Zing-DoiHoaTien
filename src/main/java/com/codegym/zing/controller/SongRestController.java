52710ff54c21e473cb88471a74bd5bd83ad786be
Tran Duc Quy <tranducquylhp@gmail.com>
2019-12-07 22:32:17 +0700

    fix create date

--- src/main/java/com/codegym/zing/controller/SongRestController.java
+++ src/main/java/com/codegym/zing/controller/SongRestController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
@@ -30,6 +31,7 @@ public class SongRestController {
 
     @PostMapping("/songs")
     public ResponseEntity<Void> createSong(@RequestBody Song song) {
+        song.setCreateDate(LocalDate.now());
         songService.save(song);
         return new ResponseEntity<>(HttpStatus.OK);
     }
