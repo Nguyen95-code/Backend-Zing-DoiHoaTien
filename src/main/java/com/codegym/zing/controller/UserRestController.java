1d93eadda04e499314ca7bd57a9d9e5216903879
Tran Duc Quy <tranducquylhp@gmail.com>
2019-12-16 11:34:12 +0700

    lay danh sach playlist cua user

--- src/main/java/com/codegym/zing/controller/UserRestController.java
+++ src/main/java/com/codegym/zing/controller/UserRestController.java
@@ -1,5 +1,6 @@
 package com.codegym.zing.controller;
 
+import com.codegym.zing.model.Playlist;
 import com.codegym.zing.model.User;
 import com.codegym.zing.service.PlaylistService;
 import com.codegym.zing.service.UserService;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +33,15 @@ public class UserRestController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}/playlists")
+    public ResponseEntity<List<Playlist>> findAllPlaylist(@PathVariable Long id){
+        User user = userService.findById(id);
+        if (user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        List<Playlist> playlists = playlistService.findAllByUser(user);
+        return new ResponseEntity<>(playlists, HttpStatus.OK);
     }
 }
