7392c1674c809512ef17de7c9cf0fc01882e7f8a
Trần Đức Quý <40483832+tranducquylhp@users.noreply.github.com>
2019-12-25 00:35:50 +0700

    edit post song

--- src/main/java/com/codegym/zing/controller/SongRestController.java
+++ src/main/java/com/codegym/zing/controller/SongRestController.java
@@ -31,10 +31,10 @@ public class SongRestController {
     }
 
     @PostMapping("/songs")
-    public ResponseEntity<Void> createSong(@RequestBody Song song) {
+    public ResponseEntity<Song> createSong(@RequestBody Song song) {
         song.setCreateDate(LocalDate.now());
         songService.save(song);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<Song>(song, HttpStatus.OK);
     }
 
     @GetMapping("/songs/{id}")
